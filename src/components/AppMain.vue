55f02d6434893d19f63c7772e29d322bb1c4f126
Eduard Condurache <eduardcondurache96@gmail.com>
2024-11-05 17:59:55 +0100

    Completata milestone 4

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,9 +5,23 @@ import axios from 'axios';
 export default {
     data() {
         return {
-            
+            projects: []
         };
     },
+    mounted () {
+        this.getProjects();
+    },
+    methods: {
+        getProjects() {
+            axios
+                .get('http://127.0.0.1:8000/api/projects')
+                .then((resp) => {
+                    console.log(resp.data);
+                    this.projects = resp.data.projects.data;
+                    console.log(this.projects);
+                });
+        }
+    }
 };
 </script>
 
