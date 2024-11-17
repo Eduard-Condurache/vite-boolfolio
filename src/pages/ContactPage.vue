91c1b461d94198ea78cd6dcb9374fa67fc5d2cc2
Eduard Condurache <eduardcondurache96@gmail.com>
2024-11-17 17:17:00 +0100

    Creato form e richiesta in post verso il backend

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,10 +1,34 @@
 <script>
+import axios from 'axios';
 export default {
     data() {
         return {
-            
+            userName: '',
+            userEmail: '',
         };
     },
+    methods: {
+        sendContact() {
+            axios
+                .post('http://localhost:8000/api/new-contact', {
+                    name: this.userName,
+                    email: this.userEmail
+                })
+                .then(res => {
+                    if(res.data.success) {
+                        alert('Success')
+
+                        this.userName = '';
+                        this.userEmail = '';
+                    } else {
+                        alert('Fail')
+                    }
+                })
+                .catch(err => {
+                    alert(err.response.data.message);
+                });
+        }
+    }
 };
 </script>
 
@@ -14,6 +38,28 @@ export default {
             Contact Page
         </h2>
 
+        <form @submit.prevent="sendContact()" class="row g-3">
+            <div class="col-md-6">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" v-model="userEmail" id="email" minlength="6" maxlength="255" placeholder="Inserisci la mail" required>
+            </div>
+            <div class="col-md-6">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" v-model="userName" id="name" placeholder="Inserisci il tuo nome" minlength="3" maxlength="64" required>
+            </div>
+           
+            <div class="col-12">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="gridCheck">
+                <label class="form-check-label" for="gridCheck">
+                    Check me out
+                </label>
+                </div>
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Sign in</button>
+            </div>
+        </form>
     </div>
 </template>
 
